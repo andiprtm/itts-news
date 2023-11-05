d40398df356c50dc00a18abd10e0552c8a960d0e
andiprtm <andipratama15011@gmail.com>
2023-11-05 17:43:57 +0700

    feat: add listNew page

--- screens/listNews.js
+++ screens/listNews.js
@@ -9,8 +9,14 @@ import {
   Dimensions,
   Image,
 } from "react-native";
+import * as Font from 'expo-font';
+import Separator from "../components/separator";
 
 const windowWidth = Dimensions.get("window").width;
+
+const poppinsMedium = require('../assets/fonts/Poppins-Medium.ttf');
+const poppinsSemiBold = require('../assets/fonts/Poppins-SemiBold.ttf');
+const poppinsBold = require('../assets/fonts/Poppins-Bold.ttf');
 
 class ListNews extends PureComponent {
   constructor(props) {
@@ -19,7 +25,17 @@ class ListNews extends PureComponent {
     this.state = {
       data: [],
       isLoading: true,
+      fontsLoaded: false,
     };
+  }
+
+  async loadFonts() {
+    await Font.loadAsync({
+        'Poppins-Medium': poppinsMedium,
+        'Poppins-SemiBold': poppinsSemiBold,
+        'Poppins-Bold': poppinsBold,
+    });
+    this.setState({ fontsLoaded: true });
   }
 
   async getNews() {
@@ -38,9 +54,11 @@ class ListNews extends PureComponent {
 
   componentDidMount() {
     this.getNews();
+    this.loadFonts();
   }
 
   renderItem = ({ item }) => {
+    key = item.id
     const { navigation } = this.props;
 
     return (
@@ -49,9 +67,15 @@ class ListNews extends PureComponent {
           style={styles.item}
           onPress={() => navigation.navigate("NewsDetail", { data: item })}
         >
-          <View style={{ flexDirection: "row", alignItems: "flex-start" }}>
+          <View style={styles.parent}>
             <Image source={{ uri: item.image }} style={styles.itemImage} />
-            <Text style={styles.itemText}>{item.title}</Text>
+            <View style={{ alignSelf: "stretch"}}>
+                <Text ellipsizeMode='tail' numberOfLines={1} style={styles.itemText}>{item.title}</Text>
+                <Separator height={8}/>
+                <View style={{borderWidth: 1, borderColor:'black', marginLeft: 15, width: "25%"}}></View>
+                <Separator height={5}/>
+                <Text ellipsizeMode='tail' numberOfLines={4} style={styles.itemContent}>{item.content}</Text>
+            </View>
           </View>
         </TouchableOpacity>
         <View style={styles.itemBorder}></View>
@@ -62,19 +86,23 @@ class ListNews extends PureComponent {
   render() {
     const { data, isLoading } = this.state;
 
-    return (
-      <View style={styles.container}>
-        {isLoading ? (
-          <ActivityIndicator size="large" color="#AA0002" />
-        ) : (
-          <FlatList
-            data={data}
-            keyExtractor={({ link }, index) => link}
-            renderItem={this.renderItem}
-          />
-        )}
-      </View>
-    );
+    if (this.state.fontsLoaded) {
+        return (
+            <View style={styles.container}>
+              {isLoading ? (
+                <ActivityIndicator size="large" color="#AA0002" />
+              ) : (
+                <FlatList
+                  data={data}
+                  keyExtractor={(item) => item.id}
+                  renderItem={this.renderItem}
+                />
+              )}
+            </View>
+          );     
+    }else{
+        return null;
+    }
   }
 }
 
@@ -86,7 +114,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   item: {
-    padding: 20,
+    paddingTop: 20,
     backgroundColor: "white",
   },
   itemBorder: {
@@ -94,12 +122,29 @@ const styles = StyleSheet.create({
     borderColor: "#cccccc",
   },
   itemImage: {
-    width: 100,
-    height: 80,
+    width: "30%",
+    height: "100%",
   },
   itemText: {
-    fontSize: 20,
+    fontFamily: "Poppins-Bold",
+    fontSize: 16,
     width: windowWidth - 150,
     marginLeft: 15,
   },
+  itemContent: {
+    fontFamily: "Poppins-Medium",
+    fontSize: 13,
+    width: windowWidth - 150,
+    marginLeft: 15,
+    marginBottom: 12
+  },
+  parent:{
+    flex: 1, 
+    flexDirection: "row", 
+    alignItems: "flex-start", 
+    width: "100%", 
+    overflow: "hidden", 
+    justifyContent: "space-between", 
+    paddingRight: "10%"
+  }
 });
