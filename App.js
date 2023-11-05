72679a112ebdef791ce003ae40b607dccd2790a8
andiprtm <andipratama15011@gmail.com>
2023-11-05 08:46:25 +0700

    feat: add font

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/button';
+import Biodata from './components/biodata';
+import LineSeparator from './components/lineSeparator';
+import Separator from './components/separator';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LineSeparator />
+      <Biodata title="Nama lengkap" text="Moch. Andi Divangga Pratama"/>
+      <LineSeparator />
+      <Biodata title="Program Studi" text="Moch. Andi Divangga Pratama"/>
+      <LineSeparator />
+      <Biodata title="Nomor Induk Mahasiswa" text="Moch. Andi Divangga Pratama"/>
+      <LineSeparator />
+      <Biodata title="Alamat Email" text="Moch. Andi Divangga Pratama"/>
+      <LineSeparator />
+      <Separator height={30}/>
+      <Button text="Logout"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +30,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: "8%",
   },
 });
